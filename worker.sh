2001bbd1e051bf571ebf4c6320a358faf284a1be
pingbay <hello@pingbay.com>
2019-10-29 21:22:30 -0400

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -1,1 +1,215 @@
+#!/bin/bash
 
+POD_CIDR="172.16.0.0/24"
+HOSTNAME=$(hostaname -s)
+TEMP_LOCAL_IP=$(ip add | grep 172.16 | awk '{print($2)}' | rev | cut -c4- | rev)
+
+# change hostname
+if [[ $TEMP_LOCAL_IP == '172.16.0.21' ]]; then
+  hostnamectl set-hostname worker01
+  TEMP_LOCAL_HOSTNAME='worker01'
+elif [[ $TEMP_LOCAL_IP == '172.16.0.22' ]]; then
+  hostnamectl set-hostname worker02
+  TEMP_LOCAL_HOSTNAME='worker02'
+elif [[ $TEMP_LOCAL_IP == '172.16.0.23' ]]; then
+  hostnamectl set-hostname worker03
+  TEMP_LOCAL_HOSTNAME='worker03'
+fi
+
+# Add DNS records
+echo 172.16.0.11  master01 >> /etc/hosts
+echo 172.16.0.12  master02 >> /etc/hosts
+echo 172.16.0.21  worker01 >> /etc/hosts
+echo 172.16.0.22  worker02 >> /etc/hosts
+echo 172.16.0.23  worker03 >> /etc/hosts
+
+# update packages
+yum update -y
+yum install -y wget net-tools tcpdump git
+
+# disable FW
+systemctl stop firewalld
+systemctl disable firewalld
+
+# Turn off swap and update fstab
+swapoff -a
+sed -i  '/swap/d' /etc/fstab
+
+yum install -y socat conntrack ipset
+
+wget -q \
+  https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.15.0/crictl-v1.15.0-linux-amd64.tar.gz \
+  https://github.com/opencontainers/runc/releases/download/v1.0.0-rc8/runc.amd64 \
+  https://github.com/containernetworking/plugins/releases/download/v0.8.2/cni-plugins-linux-amd64-v0.8.2.tgz \
+  https://github.com/containerd/containerd/releases/download/v1.2.9/containerd-1.2.9.linux-amd64.tar.gz \
+  https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kubectl \
+  https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kube-proxy \
+  https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kubelet
+
+mkdir -p \
+  /etc/cni/net.d \
+  /opt/cni/bin \
+  /var/lib/kubelet \
+  /var/lib/kube-proxy \
+  /var/lib/kubernetes \
+  /var/run/kubernetes
+  
+mkdir containerd
+tar -xvf crictl-v1.15.0-linux-amd64.tar.gz
+tar -xvf containerd-1.2.9.linux-amd64.tar.gz -C containerd
+tar -xvf cni-plugins-linux-amd64-v0.8.2.tgz -C /opt/cni/bin/
+mv runc.amd64 runc
+chmod +x crictl kubectl kube-proxy kubelet runc 
+mv crictl kubectl kube-proxy kubelet runc /usr/local/bin/
+mv containerd/bin/* /bin/ 
+
+# Create the bridge network configuration file
+cat <<EOF | sudo tee /etc/cni/net.d/10-bridge.conf
+{
+    "cniVersion": "0.3.1",
+    "name": "bridge",
+    "type": "bridge",
+    "bridge": "cnio0",
+    "isGateway": true,
+    "ipMasq": true,
+    "ipam": {
+        "type": "host-local",
+        "ranges": [
+          [{"subnet": "${POD_CIDR}"}]
+        ],
+        "routes": [{"dst": "0.0.0.0/0"}]
+    }
+}
+EOF
+
+# Create the loopback network configuration file
+cat <<EOF | sudo tee /etc/cni/net.d/99-loopback.conf
+{
+    "cniVersion": "0.3.1",
+    "name": "lo",
+    "type": "loopback"
+}
+EOF
+
+# Create the containerd configuration file
+mkdir -p /etc/containerd/
+
+cat << EOF | sudo tee /etc/containerd/config.toml
+[plugins]
+  [plugins.cri.containerd]
+    snapshotter = "overlayfs"
+    [plugins.cri.containerd.default_runtime]
+      runtime_type = "io.containerd.runtime.v1.linux"
+      runtime_engine = "/usr/local/bin/runc"
+      runtime_root = ""
+EOF
+
+# Create the containerd.service systemd unit file
+cat <<EOF | sudo tee /etc/systemd/system/containerd.service
+[Unit]
+Description=containerd container runtime
+Documentation=https://containerd.io
+After=network.target
+
+[Service]
+ExecStartPre=/sbin/modprobe overlay
+ExecStart=/bin/containerd
+Restart=always
+RestartSec=5
+Delegate=yes
+KillMode=process
+OOMScoreAdjust=-999
+LimitNOFILE=1048576
+LimitNPROC=infinity
+LimitCORE=infinity
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Configure the Kubelet
+mv ${HOSTNAME}-key.pem ${HOSTNAME}.pem /var/lib/kubelet/
+mv ${HOSTNAME}.kubeconfig /var/lib/kubelet/kubeconfig
+mv ca.pem /var/lib/kubernetes/
+
+# Create the kubelet-config.yaml configuration file
+cat <<EOF | sudo tee /var/lib/kubelet/kubelet-config.yaml
+kind: KubeletConfiguration
+apiVersion: kubelet.config.k8s.io/v1beta1
+authentication:
+  anonymous:
+    enabled: false
+  webhook:
+    enabled: true
+  x509:
+    clientCAFile: "/var/lib/kubernetes/ca.pem"
+authorization:
+  mode: Webhook
+clusterDomain: "cluster.local"
+clusterDNS:
+  - "10.32.0.10"
+podCIDR: "${POD_CIDR}"
+resolvConf: "/run/systemd/resolve/resolv.conf"
+runtimeRequestTimeout: "15m"
+tlsCertFile: "/var/lib/kubelet/${HOSTNAME}.pem"
+tlsPrivateKeyFile: "/var/lib/kubelet/${HOSTNAME}-key.pem"
+EOF
+
+# Create the kubelet.service systemd unit file
+cat <<EOF | sudo tee /etc/systemd/system/kubelet.service
+[Unit]
+Description=Kubernetes Kubelet
+Documentation=https://github.com/kubernetes/kubernetes
+After=containerd.service
+Requires=containerd.service
+
+[Service]
+ExecStart=/usr/local/bin/kubelet \\
+  --config=/var/lib/kubelet/kubelet-config.yaml \\
+  --container-runtime=remote \\
+  --container-runtime-endpoint=unix:///var/run/containerd/containerd.sock \\
+  --image-pull-progress-deadline=2m \\
+  --kubeconfig=/var/lib/kubelet/kubeconfig \\
+  --network-plugin=cni \\
+  --register-node=true \\
+  --v=2
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Configure the Kubernetes Proxy
+mv kube-proxy.kubeconfig /var/lib/kube-proxy/kubeconfig
+
+# Create the kube-proxy-config.yaml configuration file
+cat <<EOF | sudo tee /var/lib/kube-proxy/kube-proxy-config.yaml
+kind: KubeProxyConfiguration
+apiVersion: kubeproxy.config.k8s.io/v1alpha1
+clientConnection:
+  kubeconfig: "/var/lib/kube-proxy/kubeconfig"
+mode: "iptables"
+clusterCIDR: "10.200.0.0/16"
+EOF
+
+# Create the kube-proxy.service systemd unit file
+cat <<EOF | sudo tee /etc/systemd/system/kube-proxy.service
+[Unit]
+Description=Kubernetes Kube Proxy
+Documentation=https://github.com/kubernetes/kubernetes
+
+[Service]
+ExecStart=/usr/local/bin/kube-proxy \\
+  --config=/var/lib/kube-proxy/kube-proxy-config.yaml
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Start the Worker Services
+systemctl daemon-reload
+systemctl enable containerd kubelet kube-proxy
+systemctl start containerd kubelet kube-proxy
