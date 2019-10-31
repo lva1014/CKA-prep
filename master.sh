fb6dfeb6fbf63b6f95a771a5c84962a05746826b
pingbay <hello@pingbay.com>
2019-10-31 15:39:16 -0400

    Update master.sh

--- master.sh
+++ master.sh
@@ -540,12 +540,20 @@ Documentation=https://github.com/coreos
 Type=notify
 ExecStart=/usr/local/bin/etcd \\
   --name $ETCD_NAME \\
-  --initial-advertise-peer-urls http://$INTERNAL_IP:2380 \\
-  --listen-peer-urls http://$INTERNAL_IP:2380 \\
-  --listen-client-urls http://$INTERNAL_IP:2379,https://127.0.0.1:2379 \\
-  --advertise-client-urls http://$INTERNAL_IP:2379 \\
+  --cert-file=/etc/etcd/kubernetes.pem \\
+  --key-file=/etc/etcd/kubernetes-key.pem \\
+  --peer-cert-file=/etc/etcd/kubernetes.pem \\
+  --peer-key-file=/etc/etcd/kubernetes-key.pem \\
+  --trusted-ca-file=/etc/etcd/ca.pem \\
+  --peer-trusted-ca-file=/etc/etcd/ca.pem \\
+  --peer-client-cert-auth \\
+  --client-cert-auth \\
+  --initial-advertise-peer-urls https://$INTERNAL_IP:2380 \\
+  --listen-peer-urls https://$INTERNAL_IP:2380 \\
+  --listen-client-urls https://$INTERNAL_IP:2379,https://127.0.0.1:2379 \\
+  --advertise-client-urls https://$INTERNAL_IP:2379 \\
   --initial-cluster-token etcd-cluster-0 \\
-  --initial-cluster master01=http://172.16.0.11:2380,master02=http://172.16.0.12:2380,master03=http://172.16.0.13:2380 \\
+  --initial-cluster master01=https://172.16.0.11:2380,master02=https://172.16.0.12:2380,master03=https://172.16.0.13:2380 \\
   --initial-cluster-state new \\
   --data-dir=/var/lib/etcd
 Restart=on-failure
@@ -575,12 +583,20 @@ Documentation=https://github.com/coreos
 Type=notify
 ExecStart=/usr/local/bin/etcd \\
   --name $ETCD_NAME \\
-  --initial-advertise-peer-urls http://$INTERNAL_IP:2380 \\
-  --listen-peer-urls http://$INTERNAL_IP:2380 \\
-  --listen-client-urls http://$INTERNAL_IP:2379,https://127.0.0.1:2379 \\
-  --advertise-client-urls http://$INTERNAL_IP:2379 \\
+  --cert-file=/etc/etcd/kubernetes.pem \\
+  --key-file=/etc/etcd/kubernetes-key.pem \\
+  --peer-cert-file=/etc/etcd/kubernetes.pem \\
+  --peer-key-file=/etc/etcd/kubernetes-key.pem \\
+  --trusted-ca-file=/etc/etcd/ca.pem \\
+  --peer-trusted-ca-file=/etc/etcd/ca.pem \\
+  --peer-client-cert-auth \\
+  --client-cert-auth \\
+  --initial-advertise-peer-urls https://$INTERNAL_IP:2380 \\
+  --listen-peer-urls https://$INTERNAL_IP:2380 \\
+  --listen-client-urls https://$INTERNAL_IP:2379,https://127.0.0.1:2379 \\
+  --advertise-client-urls https://$INTERNAL_IP:2379 \\
   --initial-cluster-token etcd-cluster-0 \\
-  --initial-cluster master01=http://172.16.0.11:2380,master02=http://172.16.0.12:2380,master03=http://172.16.0.13:2380 \\
+  --initial-cluster master01=https://172.16.0.11:2380,master02=https://172.16.0.12:2380,master03=https://172.16.0.13:2380 \\
   --initial-cluster-state new \\
   --data-dir=/var/lib/etcd
 Restart=on-failure
