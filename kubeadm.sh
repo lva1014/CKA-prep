b8a58c54bfacb1930f04b1a3303089b65905d05c
pingbay <hello@pingbay.com>
2020-09-27 22:03:04 -0400

    Update kubeadm.sh

--- kubeadm.sh
+++ kubeadm.sh
@@ -1,20 +1,15 @@
 # CentOS
-# disable FW
+hostnamectl set-hostname <>
 systemctl stop firewalld
 systemctl disable firewalld
-
-# Turn off swap and update fstab
 swapoff -a
 sed -i  '/swap/d' /etc/fstab
-
-lsmod | grep br_netfilter
-
+sudo modprobe br_netfilter
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
 sudo sysctl --system
-
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -25,16 +20,20 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 exclude=kubelet kubeadm kubectl
 EOF
-
-# Set SELinux in permissive mode (effectively disabling it)
 sudo setenforce 0
 sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
-
-sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
-
+sudo yum install -y docker kubelet kubeadm kubectl --disableexcludes=kubernetes
 sudo systemctl enable --now kubelet
+systemctl daemon-reload
+systemctl restart kubelet
 
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo sysctl -p /etc/sysctl.conf
 
+sudo systemctl enable service.docker
+sudo systemctl start service.docker
+
+usermod -aG root ubuntu
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
